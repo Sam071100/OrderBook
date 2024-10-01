9af9e5e3a5ae7b26519233de879e90e859320e76
Shubham Samrat <shubhamsamrat382@gmail.com>
2024-10-01 23:46:13 +0900

    Error while cancelling order as we are checking null

--- orderBook.cpp
+++ orderBook.cpp
@@ -277,9 +277,45 @@ private:
             }
         }
 
+        Trades MatchOrder(OrderModify order)
+        {
+            if(!orders_.contains(order.GetOrderId()))
+                return { };
+            const auto& [existingOrder, _] = orders_.at(order.GetOrderId());
+            CancelOrder(order.GetOrderId());
+            return AddOrder(order.ToOderPointer(existingOrder->GetOrderType()));
+        }
+
+        std::size_t Size() const { return orders_.size(); }
+
+        OrderbookLevelInfos GetOrderInfos() const
+        {
+            LevelInfos bidInfos, askInfos;
+            bidInfos.reserve(orders_.size());
+            askInfos.reserve(orders_.size());
+
+            auto CreateLevelInfos = [](Price price, const OrderPointers& orders)
+            {
+                return LevelInfo{ price, std::accumulate(orders.begin(), orders.end(), (Quantity)0, [](Quantity runningSum, const OrderPointer& order) { return runningSum + order->GetRemainingQuantity(); }) };
+            };
+
+            for(const auto& [price, orders]: bids_)
+                bidInfos.push_back(CreateLevelInfos(price, orders));
+            
+            for(const auto& [price, orders]: asks_)
+                bidInfos.push_back(CreateLevelInfos(price, orders));
+
+            return OrderbookLevelInfos { bidInfos, askInfos };
+        }
 };
 
 int main()
 {
+    Orderbook orderbook;
+    const OrderId orderId = 1;
+    orderbook.AddOrder(std::make_shared<Order>(OrderType::GoodTillCancel, orderId, Side::Buy, 100, 10));
+    std::cout << orderbook.Size() << std::endl;
+    orderbook.CancelOrder(orderId);
+    std::cout << orderbook.Size() << std::endl;
     return 0;
 }
